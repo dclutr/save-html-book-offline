7ba702f05b57911c5efd79aaf338e6e6db4eb979
dclutr <81961007+dclutr@users.noreply.github.com>
2022-09-11 22:20:45 +0530

    Update save-html-book-offline.sh

--- save-html-book-offline.sh
+++ save-html-book-offline.sh
@@ -21,7 +21,7 @@
 
 function parse-links()
 {
-  echo "height $((--height))"
+  echo "height $((height - 1))"
   for attribute in href src
   do 
     # attribute="value (or) attribute=value
@@ -29,10 +29,10 @@ function parse-links()
 "$attribute=[\"][^\"]*[\"]
 $attribute=[\'][^\']*[\']
 $attribute=[^\"\'][^> ]*"
-    )
+    ) # href="a.html" or href='a.html' or href=a.html
     do
       echo "found $match"
-      url="$( unquote "$( trim-left "$match" "$(("${#attribute}"+1))" )" )"
+      url="$( sds-to-s "$( unquote "$( trim-left "$match" "$(("${#attribute}"+1))" )" )" )"
       case "$url" in
         "mailto:"*)    continue;; # ignore mailto URL
         "#"*)          continue;; # ignore fragment-only URL
@@ -48,12 +48,16 @@ $attribute=[^\"\'][^> ]*"
       then
         local_url="$( slashes-to-dots "$( remove-scheme "$url" )" )"
         swap-html "$match" "$attribute=\"$local_url\""
-        push "$height" "$( remove-fragment "$local_url" )" "$( remove-fragment "$url" )"
+	push \
+          "$((height - 1))" \
+          "$( remove-query "$( remove-fragment "$local_url" )" )" \
+          "$( remove-query "$( remove-fragment "$url" )" )"
       fi
     done
   done
 }
 
+# abc/xyz => abc
 function get-domain()
 {
   local i=0
@@ -68,6 +72,7 @@ function get-domain()
   echo "$1/"
 }
 
+# ../b.html + https://xyz.com/p/a.html => https://xyz.com/b.html
 function url-for-outer-dir()
 {
   local out=0
@@ -86,6 +91,7 @@ function url-for-outer-dir()
   echo "${2:0:i}/$url"
 }
 
+# b.html + https://xyz.com/a.html => https://xyz.com/b.html
 function url-for-same-dir()
 {
   local i="${#2}"
@@ -99,15 +105,15 @@ function url-for-same-dir()
 function push()
 {
   if [[ "$1" = -1 ]]
-  then
-    return
+  then return # page height < 0
   fi
-  if [ -z "$( cat ".history" | grep "$2#" )" ]
+  # push if filename not in history
+  if [ -z "$( cat ".history" | grep "#$2#" )" ]
   then
     echo "$1" >> ".height-stack"
     echo "$2" >> ".filename-stack"
     echo "$3" >> ".url-stack"
-    echo "$2#$1" >> ".history"
+    add-to-history "$@"
     echo "push() $1 $3"
   fi
 }
@@ -138,6 +144,8 @@ function swap-html()
   echo "swapping $1 ==> $2"
 }
 
+# 1. make book directory and change to book directory
+# 2. create files to track progress
 function init-book()
 {
   mkdir "$1"
@@ -151,13 +159,17 @@ function init-book()
 function get-title()
 {
   local title
+
+  # <title> abc </title> => abc
   title="$( alnum-words "$( trim "$( echo "$html" | grep "<title>[^<]*</title>" )" 7 8 )" )"
   if [ ! -z "$title" ]
   then
     echo "$title"
     return
   fi
+
   local line="$( echo "$html" | grep -n "<title" )"
+  # <title>\nabc => abc
   title="$( alnum-words "$( echo "$html" | head -n "$(("${line/":"*}" + 1))" | tail -n 1 )" )"
   if [ ! -z "$title" ]
   then
@@ -167,7 +179,7 @@ function get-title()
   echo "default title"
 }
 
-
+# abc/ => abc/index.html
 function index-if-dir()
 {
   case "$1" in
@@ -176,6 +188,7 @@ function index-if-dir()
   esac
 }
 
+# "abc" or 'abc' => abc
 function unquote()
 {
   case "$1" in
@@ -185,37 +198,58 @@ function unquote()
   esac
 }
 
+# xyz//abc.com => abc.com
 function remove-scheme()
 {
   echo "${1/*"//"}"
 }
 
+# a.com/b#c => a.com/b
 function remove-fragment()
 {
   echo "${1/"#"*}"
 }
 
+# a.com/b?c => a.com/b
+function remove-query()
+{
+  echo "${1/"?"*}"
+}
+
+# a.com/b/c.html => a.com.b.c.html
 function slashes-to-dots()
 {
   echo "${1//\//\.}"
 }
 
+# skit 1 => kit 
 function trim-left()
 {
   echo "${1:$2}"
 }
 
+# skittle 1 3 => kit 
 function trim()
 {
   echo "${1:$2:-$3}"
 }
 
+# rock*'s diary => rocks diary
 function alnum-words()
 {
   echo "${1//[^[:alnum:] ]}"
 }
 
+function add-to-history()
+{
+  echo "$1#$2#$3" >> ".history"
+}
 
+# /./ => /
+function sds-to-s()
+{
+  echo "${1//\/\.\////}"
+}
 
 # default height = 1
 if [ -z "$2" ]
@@ -234,10 +268,12 @@ else
   html="$( curl "$download_url" )"
   init-book "$( get-title "$html" )"
   parse-links
-  for file in "index.html" "$( slashes-to-dots "$( remove-scheme "$download_url" )" )"
+  for file in \
+    "index.html" \
+    "$( slashes-to-dots "$( remove-scheme "$download_url" )" )"
   do
     echo "$html" > "$file"
-    echo "$download_url#$height" > ".history"
+    add-to-history "$height#$file#$download_url"
   done
   while pop # pops height download_url download_target
   do   
